82710eceb3775fb3551191267529a135731a7ca0
Giorgi Dogadze <58903449+giorgi-dogadze@users.noreply.github.com>
2022-04-20 18:01:48 +0400

    feat/ create use interface

---
+++ src/core/types.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: string;
+  slug: string;
+  email: string;
+  passwordHash: string;
+  name: string;
+  lastName: string;
+  personalNumber: string;
+  phone: string;
+  createdAt: string;
+  updatedAt: string;
+}
