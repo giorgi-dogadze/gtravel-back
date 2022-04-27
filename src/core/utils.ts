8da92d4f0434a0f0b3ec07801c55e27dee441735
Giorgi Dogadze <58903449+giorgi-dogadze@users.noreply.github.com>
2022-04-27 18:39:17 +0400

    feat/ create function slugify

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -30,3 +30,15 @@ export const ActiveUser = createParamDecorator(
     return typeof request.user === 'object' ? request.user : null;
   },
 );
+
+export function slugify(name: string): string {
+  return name
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, ''); // Trim - from end of text
+}
