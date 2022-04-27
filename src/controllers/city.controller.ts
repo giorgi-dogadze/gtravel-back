c9626e393096876f02b3a8cc1c99a594a2bc3257
Giorgi Dogadze <58903449+giorgi-dogadze@users.noreply.github.com>
2022-04-27 14:54:23 +0400

    feat/ create CityController

---
+++ src/controllers/city.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { ResultList, City } from 'src/core/types';
+import { CityService } from 'src/services/city.service';
+
+@Controller()
+export class CityController {
+  constructor(private readonly service: CityService) {}
+
+  @Get('/cities')
+  async findAll(): Promise<ResultList<City>> {
+    const resultList = await this.service.findAll();
+
+    return resultList;
+  }
+}
