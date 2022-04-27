9936ed6e8cfeb20e15bba008615fed5ba36de773
Giorgi Dogadze <58903449+giorgi-dogadze@users.noreply.github.com>
2022-04-27 14:22:55 +0400

    feat/ create TravelScheduleEntity

---
+++ src/core/entities/travel-schedule.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+import { TravelSchedule, TransportType } from '../types';
+
+@Entity({ name: 'regions' })
+export class TravelScheduleEntity implements TravelSchedule {
+  @PrimaryColumn({ type: 'uuid' })
+  id!: string;
+
+  @Column({ type: 'varchar', unique: true })
+  slug!: string;
+
+  @Column({ type: 'varchar', name: 'type' })
+  type!: TransportType;
+
+  @Column({ type: 'varchar', name: 'workDays' })
+  workDays!: string;
+
+  @Column({ type: 'varchar', name: 'workYours' })
+  workYours!: string;
+
+  @Column({
+    type: 'decimal',
+    precision: 7,
+    scale: 1,
+    name: 'ticketPrice',
+  })
+  ticketPrice!: number;
+
+  @Column({ type: 'timestamp', name: 'created_at' })
+  createdAt!: string;
+
+  @Column({ type: 'timestamp', name: 'updated_at' })
+  updatedAt!: string;
+}
