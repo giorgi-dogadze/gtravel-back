78eb7652a727393f256c3c53b1d781b91508d479
Giorgi Dogadze <58903449+giorgi-dogadze@users.noreply.github.com>
2022-05-01 23:27:24 +0400

    feat/ modify city data

--- src/data/city.data.ts
+++ src/data/city.data.ts
@@ -2,6 +2,8 @@ import { City } from 'src/core/types';
 import { slugify } from 'src/core/utils';
 import { v4 as uuid } from 'uuid';
 import { formatISO } from 'date-fns';
+import { gazafxuli, kalauri, darkvetis } from './restaurant.data';
+import { hotel1, hotel2, hotel3 } from './hotel.data';
 
 export const chiaturaId = uuid();
 export const sachkhereId = uuid();
@@ -21,6 +23,8 @@ export const chiatura: City = {
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Katskhi_Pillar.jpg/250px-Katskhi_Pillar.jpg',
   },
   history: 'დაარსა აკაკიმ 1879წელს',
+  hotels: [hotel1, hotel3],
+  restaurants: [gazafxuli],
   createdAt: formatISO(new Date()),
   updatedAt: formatISO(new Date()),
 };
@@ -39,6 +43,8 @@ export const sachkhere: City = {
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Katskhi_Pillar.jpg/250px-Katskhi_Pillar.jpg',
   },
   history: 'დაარსა აკაკიმ 1879წელს',
+  hotels: [hotel1, hotel2, hotel3],
+  restaurants: [],
   createdAt: formatISO(new Date()),
   updatedAt: formatISO(new Date()),
 };
@@ -57,6 +63,8 @@ export const qutaisi: City = {
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Katskhi_Pillar.jpg/250px-Katskhi_Pillar.jpg',
   },
   history: 'დაარსა აკაკიმ 1879წელს',
+  hotels: [],
+  restaurants: [gazafxuli, kalauri, darkvetis],
   createdAt: formatISO(new Date()),
   updatedAt: formatISO(new Date()),
 };
