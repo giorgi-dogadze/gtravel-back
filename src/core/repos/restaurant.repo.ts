e32ed56dba09d1e018f9a2210beca042291a01c6
Giorgi Dogadze <58903449+giorgi-dogadze@users.noreply.github.com>
2022-05-01 23:27:10 +0400

    feat/ create RestaurantRepo

---
+++ src/core/repos/restaurant.repo.ts
@@ -0,0 +1,21 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RestaurantEntity } from '../entities/restaurant.entity';
+
+@Injectable()
+export class RestaurantRepo {
+  constructor(
+    @InjectRepository(RestaurantEntity)
+    private repo: Repository<RestaurantEntity>,
+  ) {}
+
+  public async findAll(): Promise<RestaurantEntity[]> {
+    return this.repo.find();
+  }
+
+  public async save(restaurant: RestaurantEntity): Promise<RestaurantEntity> {
+    await this.repo.save(restaurant);
+    return restaurant;
+  }
+}
