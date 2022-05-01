f8f43c0b113c6dcf73a0f36f9242be0caf733975
Giorgi Dogadze <58903449+giorgi-dogadze@users.noreply.github.com>
2022-05-01 23:26:40 +0400

    feat/ create RestaurantEntity

---
+++ src/core/entities/restaurant.entity.ts
@@ -0,0 +1,42 @@
+import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Restaurant } from '../types';
+import { CityEntity } from './city.entity';
+
+@Entity({ name: 'restaurants' })
+export class RestaurantEntity implements Restaurant {
+  @PrimaryColumn({ type: 'uuid' })
+  id!: string;
+
+  @Column({ type: 'varchar', unique: true })
+  slug!: string;
+
+  @Column({ type: 'varchar' })
+  name!: string | null;
+
+  @Column({
+    type: 'decimal',
+    precision: 3,
+    scale: 2,
+    name: 'rating',
+  })
+  rating!: number;
+
+  @Column({ type: 'varchar', name: 'address' })
+  address!: string;
+
+  @Column({ type: 'uuid', name: 'image_url', array: true })
+  imageUrl!: string[];
+
+  @Column({ type: 'varchar', name: 'description' })
+  description!: string;
+
+  @ManyToOne(() => CityEntity, (city) => city.restaurants)
+  @JoinColumn({ name: 'city_id' })
+  city!: CityEntity;
+
+  @Column({ type: 'timestamp', name: 'created_at' })
+  createdAt!: string;
+
+  @Column({ type: 'timestamp', name: 'updated_at' })
+  updatedAt!: string;
+}
