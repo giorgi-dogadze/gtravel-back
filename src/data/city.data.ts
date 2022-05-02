b96ce8e0223bde3a8ba254c70cfc28e27e0fdf2a
Giorgi Dogadze <58903449+giorgi-dogadze@users.noreply.github.com>
2022-05-02 18:26:25 +0400

    feat/ add some data

--- src/data/city.data.ts
+++ src/data/city.data.ts
@@ -2,15 +2,27 @@ import { City } from 'src/core/types';
 import { slugify } from 'src/core/utils';
 import { v4 as uuid } from 'uuid';
 import { formatISO } from 'date-fns';
-import { gazafxuli, kalauri, darkvetis } from './restaurant.data';
-import { hotel1, hotel2, hotel3 } from './hotel.data';
-
-export const chiaturaId = uuid();
-export const sachkhereId = uuid();
-export const qutaisiId = uuid();
+import {
+  gazafxuli,
+  kalauri,
+  darkvetis,
+  restaurant1,
+  restaurant2,
+  restaurant3,
+  restaurant4,
+} from './restaurant.data';
+import {
+  hotel1,
+  hotel2,
+  hotel3,
+  hotel4,
+  hotel5,
+  hotel6,
+  hotel7,
+} from './hotel.data';
 
 export const chiatura: City = {
-  id: chiaturaId,
+  id: uuid(),
   slug: slugify('chiatura'),
   name: 'chiatura',
   area: 54.5,
@@ -23,14 +35,14 @@ export const chiatura: City = {
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Katskhi_Pillar.jpg/250px-Katskhi_Pillar.jpg',
   },
   history: 'დაარსა აკაკიმ 1879წელს',
-  hotels: [hotel1, hotel3],
+  hotels: [hotel1, hotel2, hotel3],
   restaurants: [gazafxuli],
   createdAt: formatISO(new Date()),
   updatedAt: formatISO(new Date()),
 };
 
 export const sachkhere: City = {
-  id: sachkhereId,
+  id: uuid(),
   slug: slugify('sachkhere'),
   name: 'sachkhere',
   area: 54.5,
@@ -43,14 +55,14 @@ export const sachkhere: City = {
       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Katskhi_Pillar.jpg/250px-Katskhi_Pillar.jpg',
   },
   history: 'დაარსა აკაკიმ 1879წელს',
-  hotels: [hotel1, hotel2, hotel3],
-  restaurants: [],
+  hotels: [hotel4, hotel5, hotel6, hotel7],
+  restaurants: [restaurant1, restaurant2, restaurant3, restaurant4, kalauri],
   createdAt: formatISO(new Date()),
   updatedAt: formatISO(new Date()),
 };
 
 export const qutaisi: City = {
-  id: qutaisiId,
+  id: uuid(),
   slug: slugify('qutaisi'),
   name: 'qutaisi',
   area: 54.5,
@@ -64,9 +76,9 @@ export const qutaisi: City = {
   },
   history: 'დაარსა აკაკიმ 1879წელს',
   hotels: [],
-  restaurants: [gazafxuli, kalauri, darkvetis],
+  restaurants: [darkvetis],
   createdAt: formatISO(new Date()),
   updatedAt: formatISO(new Date()),
 };
 
-export const cities: City[] = [chiatura, sachkhere, qutaisi];
+export const cities: City[] = [qutaisi, sachkhere, chiatura];
