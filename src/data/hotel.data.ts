c3082eca5cb84ec1e55ae3776b40293675ec9c5c
Giorgi Dogadze <58903449+giorgi-dogadze@users.noreply.github.com>
2022-05-02 18:26:37 +0400

    feat/ add some data about hotels

--- src/data/hotel.data.ts
+++ src/data/hotel.data.ts
@@ -54,4 +54,80 @@ export const hotel3: Hotel = {
   updatedAt: formatISO(new Date()),
 };
 
-export const hotels: Hotel[] = [hotel1, hotel2, hotel3];
+export const hotel4: Hotel = {
+  id: uuid(),
+  slug: slugify('hotel4'),
+  name: 'hotel4',
+  rating: 3.5,
+  address: 'univermagtan',
+  imageUrl: [
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+  ],
+  description: 'nice place',
+  price: Math.random() * 5,
+  createdAt: formatISO(new Date()),
+  updatedAt: formatISO(new Date()),
+};
+
+export const hotel5: Hotel = {
+  id: uuid(),
+  slug: slugify('hotel5'),
+  name: 'hotel5',
+  rating: 3.5,
+  address: 'univermagtan',
+  imageUrl: [
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+  ],
+  description: 'nice place',
+  price: Math.random() * 5,
+  createdAt: formatISO(new Date()),
+  updatedAt: formatISO(new Date()),
+};
+
+export const hotel6: Hotel = {
+  id: uuid(),
+  slug: slugify('hotel6'),
+  name: 'hotel6',
+  rating: 3.5,
+  address: 'univermagtan',
+  imageUrl: [
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+  ],
+  description: 'nice place',
+  price: Math.random() * 5,
+  createdAt: formatISO(new Date()),
+  updatedAt: formatISO(new Date()),
+};
+
+export const hotel7: Hotel = {
+  id: uuid(),
+  slug: slugify('hotel7'),
+  name: 'hotel7',
+  rating: 3.5,
+  address: 'univermagtan',
+  imageUrl: [
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+  ],
+  description: 'nice place',
+  price: Math.random() * 5,
+  createdAt: formatISO(new Date()),
+  updatedAt: formatISO(new Date()),
+};
+
+export const hotels: Hotel[] = [
+  hotel1,
+  hotel2,
+  hotel3,
+  hotel4,
+  hotel5,
+  hotel6,
+  hotel7,
+];
