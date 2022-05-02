8f0f4c9cf05f0484d7e02b84b373aaef46d206fa
Giorgi Dogadze <58903449+giorgi-dogadze@users.noreply.github.com>
2022-05-02 18:26:49 +0400

    feat/ add some data about restaurants

--- src/data/restaurant.data.ts
+++ src/data/restaurant.data.ts
@@ -51,4 +51,76 @@ export const darkvetis: Restaurant = {
   updatedAt: formatISO(new Date()),
 };
 
-export const restaurants: Restaurant[] = [gazafxuli, kalauri, darkvetis];
+export const restaurant1: Restaurant = {
+  id: uuid(),
+  slug: slugify('restaurant1'),
+  name: 'restaurant1',
+  rating: 3.5,
+  address: 'univermagtan',
+  imageUrl: [
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+  ],
+  description: 'nice place',
+  createdAt: formatISO(new Date()),
+  updatedAt: formatISO(new Date()),
+};
+
+export const restaurant2: Restaurant = {
+  id: uuid(),
+  slug: slugify('restaurant2'),
+  name: 'restaurant2',
+  rating: 3.5,
+  address: 'univermagtan',
+  imageUrl: [
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+  ],
+  description: 'nice place',
+  createdAt: formatISO(new Date()),
+  updatedAt: formatISO(new Date()),
+};
+
+export const restaurant3: Restaurant = {
+  id: uuid(),
+  slug: slugify('restaurant3'),
+  name: 'restaurant3',
+  rating: 3.5,
+  address: 'univermagtan',
+  imageUrl: [
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+  ],
+  description: 'nice place',
+  createdAt: formatISO(new Date()),
+  updatedAt: formatISO(new Date()),
+};
+
+export const restaurant4: Restaurant = {
+  id: uuid(),
+  slug: slugify('restaurant4'),
+  name: 'restaurant4',
+  rating: 3.5,
+  address: 'univermagtan',
+  imageUrl: [
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg',
+  ],
+  description: 'nice place',
+  createdAt: formatISO(new Date()),
+  updatedAt: formatISO(new Date()),
+};
+
+export const restaurants: Restaurant[] = [
+  gazafxuli,
+  kalauri,
+  darkvetis,
+  restaurant1,
+  restaurant2,
+  restaurant3,
+  restaurant4,
+];
